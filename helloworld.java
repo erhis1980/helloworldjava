86ec87b0d53b493da4d022bfb316d8f8540f4b3c
erhis1980 <100033157+erhis1980@users.noreply.github.com>
2022-02-26 17:49:21 +0100

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World done by my first GITHUB with Cron by Webhooks");
+    System.out.println("Hello World done by my first GITHUB with Cron by Webhooks2");
     }
 }
